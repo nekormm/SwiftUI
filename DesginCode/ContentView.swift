4e3214549139b41eb1fb43c92dbb0e2a6689e70f
Jaime Cadena <>
2021-12-13 16:47:44 -0500

    Class 6

--- DesginCode/ContentView.swift
+++ DesginCode/ContentView.swift
@@ -11,45 +11,62 @@ struct ContentView: View {
     
     @State var show : Bool = false
     @State var viewState = CGSize.zero
-    
+    @State var showCard : Bool = false
     
     var body: some View {
         ZStack {
             
             TitleView()
                 .blur(radius: show ? 20 : 0)
-                .animation(.default)
+                .opacity(showCard ? 0.4 : 1)
+                .offset(y: showCard ? -200 : 0)
+                .animation(
+                    Animation
+                        .default
+                        .delay(0.1)
+                )
             
             BackCardView()
+                .frame(width: 340, height: 220)
                 .background(Color.blue)
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x:0, y: show ? -400 : -40)
                 .offset(x: self.viewState.width, y: self.viewState.height)
-                .scaleEffect(0.9)
+                .offset(y: showCard ? -180 : 0)
+                .scaleEffect(showCard ? 1 : 0.9)
                 .rotationEffect(.degrees(show ? 0 : 10))
+                .rotationEffect(.degrees(showCard ? -10 : 0))
                 .rotation3DEffect(.degrees(10), axis: (x: 10.0, y: 0, z: 0))
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.5))
             
             BackCardView()
+                .frame(width: 340, height: 220)
                 .background(Color.blue)
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x:0, y: show ? -200 : -20)
                 .offset(x: self.viewState.width, y: self.viewState.height)
-                .scaleEffect(0.95)
+                .offset(y: showCard ? -140 : 0)
+                .scaleEffect(showCard ? 1 : 0.95)
                 .rotationEffect(.degrees(show ? 0 : 5))
+                .rotationEffect(.degrees(showCard ? -5 : 0))
                 .rotation3DEffect(.degrees(5), axis: (x: 10.0, y: 0, z: 0))
                 .blendMode(.darken)
                 .animation(.easeInOut(duration: 0.3))
 
             
             CardView()
+                .frame(width: showCard ? .infinity : 340.0, height: 220.0)
+                .background(Color.cyan)
+                .clipShape(RoundedRectangle(cornerRadius: showCard ? 30 : 20, style: .continuous))
+                .shadow(radius: 20)
                 .offset(x: self.viewState.width, y: self.viewState.height)
+                .offset(y: showCard ? -100 : 0)
                 .animation(.spring(response: 0.3 , dampingFraction: 0.45, blendDuration: 0.4))
                 .onTapGesture {
-                    self.show.toggle()
+                    self.showCard.toggle()
                 }
                 .gesture(
                     DragGesture()
@@ -63,8 +80,9 @@ struct ContentView: View {
             
             
             BottonView()
+                .offset(x:0, y: showCard ? 360 : 1000)
                 .blur(radius: show ? 20: 0)
-                .animation(.default)
+                .animation(.timingCurve(0.2, 0.8, 0.2, 1.0, duration: 0.8))
         }
     }
 }
@@ -98,10 +116,6 @@ struct CardView: View {
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 300, height: 110, alignment: .top)
         }
-        .frame(width: 340.0, height: 220.0)
-        .background(Color.cyan)
-        .cornerRadius(20)
-        .shadow(radius: 20)
         
     }
 }
@@ -111,8 +125,6 @@ struct BackCardView: View {
         VStack{
             Spacer()
         }
-        .frame(width: 340, height: 220)
-        
         
     }
 }
@@ -150,9 +162,10 @@ struct BottonView: View {
         }
         .padding(.top, 8)
         .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity)
         .background(Color.white)
         .cornerRadius(30)
         .shadow(radius: 20)
-        .offset(x:0, y:500)
+        
     }
 }
