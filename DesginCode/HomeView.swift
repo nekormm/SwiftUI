bcf0bdd304492796ae4e18ee2b89866174d21668
Jaime Cadena <>
2021-12-15 16:45:38 -0500

    Class 13

--- DesginCode/HomeView.swift
+++ DesginCode/HomeView.swift
@@ -13,16 +13,33 @@ struct HomeView: View {
     
     var body: some View {
         VStack {
+            
+            
+            
             HStack {
                 Text("Watching")
                     .font(.system(size: 28 , weight: .bold))
+                    .foregroundColor(Color.black )
                 
                 Spacer()
                 
                 AvatarView(showProfile: $showProfile)
             }
             .padding(.horizontal)
+            .padding(.leading, 14)
             .padding(.top , 30)
+            
+            
+            ScrollView (.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0 ..< 3) { item in
+                        SectionView()
+                    }
+                }
+                .padding(30)
+                .padding(.bottom, 30)
+            }
+            
             Spacer()
         }
     }
@@ -31,5 +48,36 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView(showProfile: .constant(false))
+    }
+}
+
+struct SectionView: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text("Options available")
+                    .font(.system(size: 24, weight: .semibold))
+                    .frame(width: 160, alignment: .leading)
+                
+                Spacer()
+                
+                Image("Logo1")
+                
+            }
+            
+            Text("18 options")
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image("Card6")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .frame(width: 275, height: 275)
+        .background(Color("card1"))
+        .cornerRadius(30)
+        .shadow(color: Color("card1").opacity(0.5), radius: 20, x: 0, y: 20)
     }
 }
