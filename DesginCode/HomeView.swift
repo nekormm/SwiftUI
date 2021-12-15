4e5071e55d21c3e32ff674cd71933850d948cbba
Jaime Cadena <>
2021-12-15 09:58:55 -0500

    Class 10

---
+++ DesginCode/HomeView.swift
@@ -0,0 +1,62 @@
+ //
+//  HomeView.swift
+//  DesginCode
+//
+//  Created by Jaime Cadena on 14/12/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State var showProfile : Bool = false
+    
+    
+    var body: some View {
+        ZStack {
+            
+            Color.gray
+                .edgesIgnoringSafeArea(.all )
+            
+            VStack {
+                HStack {
+                    Text("Watching")
+                        .font(.system(size: 28 , weight: .bold))
+                    
+                    Spacer()
+                    
+                    Button(action: {self.showProfile.toggle()}) {
+                        Image("Avatar")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 36, height: 36)
+                            .clipShape(Circle())
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top , 30)
+                Spacer()
+            }
+            .padding(.top , 44)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .offset(y: showProfile ? -450 : 0)
+            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10, y: 0, z: 0))
+            .scaleEffect(showProfile ? 0.9 : 1)
+            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            .edgesIgnoringSafeArea(.all)
+            
+            MenuView()
+                .offset(y: showProfile ? 0 : 600)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
