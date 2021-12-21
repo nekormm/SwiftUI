bde37a6b105025435e03e80a9d2c9f09cbabaa79
Jaime Cadena <jcadena@montran.com>
2021-12-21 13:58:56 -0500

    Class 15

--- DesginCode/HomeView.swift
+++ DesginCode/HomeView.swift
@@ -31,7 +31,16 @@ struct HomeView: View {
             ScrollView (.horizontal, showsIndicators: false) {
                 HStack {
                     ForEach(sectionData) { item in
-                        SectionView(section: item)
+                       
+                       GeometryReader { geometry in
+                          SectionView(section: item)
+                             .rotation3DEffect(Angle( degrees:
+                              Double(geometry.frame(in: .global).minX - 30 )/20
+                              
+                             ), axis: (x: 0, y: -10, z: 0))
+                       }
+                       .frame(width: 300, height: 300)
+                       
                     }
                 }
                 .padding(30)
