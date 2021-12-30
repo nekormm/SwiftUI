bd5baca702d595c867d9d669382f8874e7cf2ec3
Jaime Cadena <jcadena@montran.com>
2021-12-30 14:54:15 -0500

    Class 21

--- DesginCode/RingView.swift
+++ DesginCode/RingView.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct RingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       ZStack {
+          
+          Circle()
+             .stroke(Color.black.opacity(0.2), style: StrokeStyle(lineWidth: 5))
+             .frame(width: 44, height: 44)
+          
+          Circle()
+             .trim(from: 0.2, to: 1)
+             .stroke(
+               LinearGradient(colors: [Color.red, Color.green], startPoint: .leading, endPoint: .trailing), style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20,0], dashPhase: 5)
+             )
+             .rotationEffect(Angle(degrees: 90))
+             .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0))
+             .frame(width: 44, height: 44)
+          .shadow(color: Color.red.opacity(0.3), radius: 3, x: 0, y: 3)
+          
+          Text("80%")
+             .font(.subheadline)
+             .fontWeight(.medium)
+       }
     }
 }
 
