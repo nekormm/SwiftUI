4894fa4a3995ff90d363053509148fa2b9e08139
Jaime Cadena <jcadena@montran.com>
2022-01-03 11:07:17 -0500

    Class 22

--- DesginCode/RingView.swift
+++ DesginCode/RingView.swift
@@ -8,25 +8,34 @@
 import SwiftUI
 
 struct RingView: View {
+   
+   var color1 = (#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1))
+   var color2 = (#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1))
+   var width : CGFloat = 44
+   var height : CGFloat = 44
+   var percent : CGFloat = 82
+   
     var body: some View {
-       ZStack {
+       let multiplier = width / 44
+       let progress = 1 - percent / 100
+       return ZStack {
           
           Circle()
-             .stroke(Color.black.opacity(0.2), style: StrokeStyle(lineWidth: 5))
-             .frame(width: 44, height: 44)
+             .stroke(Color.black.opacity(0.2), style: StrokeStyle(lineWidth: 5 * multiplier))
+             .frame(width: width, height: height)
           
           Circle()
-             .trim(from: 0.2, to: 1)
+             .trim(from: progress, to: 1)
              .stroke(
-               LinearGradient(colors: [Color.red, Color.green], startPoint: .leading, endPoint: .trailing), style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20,0], dashPhase: 5)
+               LinearGradient(colors: [Color(color1), Color(color2)], startPoint: .leading, endPoint: .trailing), style: StrokeStyle(lineWidth: 5 * multiplier , lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20,0], dashPhase: 5)
              )
              .rotationEffect(Angle(degrees: 90))
              .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0))
-             .frame(width: 44, height: 44)
-          .shadow(color: Color.red.opacity(0.3), radius: 3, x: 0, y: 3)
+             .frame(width: width, height: height)
+          .shadow(color: Color(color2).opacity(0.3), radius: 3 * multiplier, x: 0, y: 3 * multiplier)
           
-          Text("80%")
-             .font(.subheadline)
+          Text("\(Int(percent))%")
+             .font(.system(size: 14 * multiplier))
              .fontWeight(.medium)
        }
     }
@@ -37,3 +46,6 @@ struct RingView_Previews: PreviewProvider {
         RingView()
     }
 }
+
+
+
